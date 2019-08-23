94787ad603b2e36d59f0cd1b15597e8fae69fad4
equiroga8 <eduqb8@gmail.com>
2019-08-23 16:22:11 +0200

    Started boggle solver assignment

---
+++ BoggleSolver.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class BoggleSolver {
+
+    // Initializes the data structure using the given array of strings as the dictionary.
+    // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
+    public BoggleSolver(String[] dictionary) {
+
+    }
+
+    // Returns the set of all valid words in the given Boggle board, as an Iterable.
+    public Iterable<String> getAllValidWords(BoggleBoard board) {
+        return null;
+    }
+
+    // Returns the score of the given word if it is in the dictionary, zero otherwise.
+    // (You can assume the word contains only the uppercase letters A through Z.)
+    public int scoreOf(String word) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
